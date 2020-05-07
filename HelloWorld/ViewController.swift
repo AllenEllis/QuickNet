6533644131ef8241ecb6026aa33b002025ec219d
Allen Ellis <allenellis@gmail.com>
2020-05-07 17:50:26 -0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Allen Ellis on 5/7/20.
+//  Copyright © 2020 Friendship Creative. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
